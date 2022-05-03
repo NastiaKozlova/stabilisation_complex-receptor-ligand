d8eda1fc0274b96bd221a406c0366b524cee4b4b
NastiaKozlova <KozlovaNastasiaSer@gmail.com>
2022-05-03 06:46:29 +0100

    same structure count

--- r_scripts/docking/r_scripts/RMSD_merge_docking_center.R
+++ r_scripts/docking/r_scripts/RMSD_merge_docking_center.R
@@ -47,7 +47,7 @@ for (q in 1:nrow(df_analysis)) {
   group_size<-round(nrow(df_structure_RMSD_TEMP)/100,digits = 0)
   if(!file.exists(paste0("RMSD_merged_center/",df_analysis$receptor_ligand[q],".csv"))){
     df_structure_RMSD_analysis<-left_join(df_structure_RMSD_TEMP,df_structure_RMSD_TEMP,by=c("receptor","ligand","RMSD"))
-    df_structure_RMSD_analysis<-df_structure_RMSD_analysis%>%filter(name.x!=name.y)
+ #   df_structure_RMSD_analysis<-df_structure_RMSD_analysis%>%filter(name.x!=name.y)
     
     for (j in 1:nrow(df_structure_RMSD_analysis)) {
       pdb_1<-read.pdb(paste0("str_fin/",df_structure_RMSD_analysis$name.x[j]))
