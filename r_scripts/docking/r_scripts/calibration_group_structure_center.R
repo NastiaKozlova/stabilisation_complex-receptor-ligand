3845409eca71d5b538a33910878ba3c9d4b36a73
NastiaKozlova <KozlovaNastasiaSer@gmail.com>
2022-06-03 10:41:56 +0100

    add sorting code

--- r_scripts/docking/r_scripts/calibration_group_structure_center.R
+++ r_scripts/docking/r_scripts/calibration_group_structure_center.R
@@ -1,15 +1,30 @@
-part_start <- commandArgs(trailingOnly=TRUE)
+part_analysis <- commandArgs(trailingOnly=TRUE)
 library(bio3d)
 library(readr)
 library(dplyr)
 library(ggplot2)
 v_rmsds<-seq(from=0,to=100,by=1)
-setwd(part_start)
-part<-paste0(part_start,"din/")
+setwd(part_analysis)
+part<-paste0(part_analysis,"din/")
 setwd(part)
 
-df_all<-read.csv(paste0(part_start,"df_all.csv"),stringsAsFactors = F)
+df_all<-read.csv(paste0(part_analysis,"df_all.csv"),stringsAsFactors = F)
 df_all<-df_all%>%mutate(name=paste0(receptor,"_",ligand,"_",center))
+df_all<-df_all%>%mutate(name=paste0(receptor,"_",ligand,"_",center))
+df_all<-df_all%>%mutate(x=NA)
+df_all<-df_all%>%mutate(y=NA)
+df_all<-df_all%>%mutate(z=NA)
+i<-1
+for (i in 1:nrow(df_all)) {
+  a<-strsplit(df_all$center[i],split = "_")[[1]]
+  df_all$x[i]<-as.numeric(a[3])
+  df_all$y[i]<-as.numeric(a[5])
+  df_all$z[i]<-as.numeric(a[7])
+}
+df_all<-df_all%>%filter(is.na(x))
+#df_all$name<-NULL
+df_all<-df_all%>%select(name,receptor,ligand,center)
+
 if(!dir.exists("df_RMSD_all")){dir.create("df_RMSD_all")}
 for (i in 1:nrow(df_all)) {
   if(!file.exists(paste0("RMSD_analysis/",df_all$name[i],".csv"))){
@@ -17,6 +32,7 @@ for (i in 1:nrow(df_all)) {
   }
 }
 df_all<-df_all%>%filter(!is.na(receptor))
+
 for (i in 1:nrow(df_all)) {
   if(!file.exists(paste0("df_RMSD_all/",df_all$name[i],".csv"))){
     df_RMSD_all<-read.csv(paste0("RMSD_analysis/",df_all$name[i],".csv"),stringsAsFactors = F)
@@ -49,9 +65,9 @@ df_RMSD_add$RMSD<-seqv
 df_RMSD_add$group<-0
 df_RMSD_all<-rbind(df_RMSD_all,df_RMSD_add)
 
-df_RMSD_all<-ungroup(df_RMSD_all)
-df_RMSD_all<-df_RMSD_all%>%select(RMSD_new,group_new)
-df_RMSD_all<-unique(df_RMSD_all)
+#df_RMSD_all<-ungroup(df_RMSD_all)
+#df_RMSD_all<-df_RMSD_all%>%select(RMSD_new,group_new)
+#df_RMSD_all<-unique(df_RMSD_all)
 p<-ggplot(data=df_RMSD_all)+
   labs(title = "ACHE")+
   geom_line(aes(x=RMSD,y=group))+
