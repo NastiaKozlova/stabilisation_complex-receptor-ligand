640f73e48e1f236f9c74a23f549ed1a3e415f7fa
NastiaKozlova <KozlovaNastasiaSer@gmail.com>
2022-06-15 06:57:47 +0100

    add complex structure merdure on the surface

---
+++ r_scripts/docking/r_scripts/complex_structure_surf.R
@@ -0,0 +1,22 @@
+part_analysis <- commandArgs(trailingOnly=TRUE)
+
+library(bio3d)
+library(dplyr)
+library(ggplot2)
+paste0(part_analysis)
+
+df_merge<-read.csv(paste0(part_analysis,"din/","df_merge_structure_log.csv"),stringsAsFactors = F)
+#df_merge<-df_merge%>%mutate(name=paste0(ligand,"_",center,"_",models.x))
+#df_merge<-semi_join(df_merge,df_all)
+df_merge<-df_merge%>%select(name.x,receptor,ligand,size_of_group)
+df_merge<-unique(df_merge)
+i<-1
+if(!dir.exists(paste0(part_analysis,"din/complex_structure_surf"))){dir.create(paste0(part_analysis,"din/complex_structure_surf"))}
+for (i in 1:nrow(df_merge)) {
+  receptor_name<-paste0(part_analysis,"receptor_start/",df_merge$receptor[i],".pdb")
+  ligand_name<-paste0(part_analysis,"din/str_fin/",df_merge$name.x[i])
+  pdb_receptor<-read.pdb(receptor_name)
+  pdb_ligand<-read.pdb(ligand_name)
+  pdb_complex<-cat.pdb(pdb_receptor, pdb_ligand, rechain=TRUE)
+  write.pdb(pdb_complex,paste0(part_analysis,"din/complex_structure_surf/",df_merge$name.x[i]))
+}
