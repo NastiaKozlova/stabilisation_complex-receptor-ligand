f017398ffeded710b6fe4bb023cc4b8cccd96510
NastiaKozlova <KozlovaNastasiaSer@gmail.com>
2022-06-20 12:15:55 +0100

    fix bugs

--- r_scripts/docking/r_scripts/complex_structure_surf.R
+++ r_scripts/docking/r_scripts/complex_structure_surf.R
@@ -11,6 +11,8 @@ df_merge<-read.csv(paste0(part_analysis,"din/","df_merge_structure_log.csv"),str
 df_merge<-df_merge%>%select(name.x,receptor,ligand,size_of_group)
 df_merge<-unique(df_merge)
 i<-1
+if(dir.exists(paste0(part_analysis,"din/complex_structure_surf"))) {system(command = paste0("rm -r ",part_analysis,"din/complex_structure_surf"),ignore.stdout=T,wait = T)}
+
 if(!dir.exists(paste0(part_analysis,"din/complex_structure_surf"))){dir.create(paste0(part_analysis,"din/complex_structure_surf"))}
 for (i in 1:nrow(df_merge)) {
   receptor_name<-paste0(part_analysis,"receptor_start/",df_merge$receptor[i],".pdb")
