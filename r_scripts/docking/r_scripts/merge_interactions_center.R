95cbebcea0e2ea579e39bb9bd2325378f1b01e75
NastiaKozlova <Kozlovaas.93@gmail.com>
2022-09-01 10:24:24 +0100

    fix plot structure

--- r_scripts/docking/r_scripts/merge_interactions_center.R
+++ r_scripts/docking/r_scripts/merge_interactions_center.R
@@ -45,3 +45,31 @@ for (j in 1:length(v_structure)) {
               paste0("interaction_center/",v_structure[j],".csv"),row.names = F)
   }
 }
+df_all<-df_all%>%select(name.x,receptor,ligand,center.x,receptor_ligand)
+df_all<-unique(df_all)
+df_all<-df_all%>%group_by(receptor_ligand)%>%mutate(number=1:n())
+df_all<-df_all%>%mutate(number=as.character(number))
+df_all<-ungroup(df_all)
+df_pdb<-read.csv(paste0("interaction_center/",df_all$name.x[1],".csv"),stringsAsFactors = F)
+df_pdb<-df_pdb%>%mutate(name.x=df_all$name.x[1])
+for (j in 2:nrow(df_all)) {
+  df_pdb_add<-read.csv(paste0("interaction_center/",df_all$name.x[j],".csv"),stringsAsFactors = F)
+  df_pdb_add<-df_pdb_add%>%mutate(name.x=df_all$name.x[j])
+  df_pdb<-rbind(df_pdb,df_pdb_add)
+}
+df_pdb<-left_join(df_pdb,df_all,by="name.x")
+df_pdb<-df_pdb%>%filter(persent_interactions==100)
+#df_pdb<-df_pdb[df_pdb$ligand%in%c("Na","HPO4"),]
+#df_pdb<-df_pdb%>%filter(receptor=="charmm-gui-1717818438")
+part_start<-strsplit(part_analysis,split = "/",fixed = T)[[1]]
+part_start<-paste0(part_start[1:(length(part_start)-3)],collapse = "/")
+part_start<-paste0(part_start,"/")
+#df_topology<-read.csv(paste0(part_start,"start/df_topology.csv"),stringsAsFactors = F)
+#v_seq<-seq(from=0,to =max(df_topology$seq_end),by=10)
+v_seq<-seq(from=0,to =10000,by=10)
+p<-ggplot()+
+  geom_point(aes(x=resno,y=number),data=df_pdb)+
+#  geom_rect(aes(xmin = seq_beg-0.5, xmax = seq_end+0.5, ymin = -Inf, ymax = Inf,fill=topology,alpha=0.1),data=df_topology)+
+  theme_bw()+facet_grid(ligand~center.x, scales = "free")+
+  scale_x_continuous(breaks = v_seq,labels = v_seq)+guides(alpha = "none")
+ggsave(p,   filename = paste0("center_interactions.png"), width = 60, height = 15, units = c("cm"), dpi = 200 ) 
