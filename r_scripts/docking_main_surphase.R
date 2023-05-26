86a084583a94f8f7ca3e07d5f58debfc6586ea1a
NastiaKozlova <kozlovaas.93gmail.com>
2023-05-26 14:00:45 -0500

    add comments

--- r_scripts/docking_main_surphase.R
+++ r_scripts/docking_main_surphase.R
@@ -18,17 +18,19 @@ system(command = paste0("Rscript --vanilla  ",part_scriprs,"prepare_first_dockin
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"first_docking_start_analysis.R ",part_name),ignore.stdout=T,wait = T)
 #log to csv
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"prepare_log_csv.R ",part_analysis),ignore.stdout=T,wait = T)
-system(command = paste0("chmod +x ",part_analysis,"prepare_log_csv.py "),ignore.stdout=T,wait = T)
-system(command = paste0("python3 ", part_analysis,"prepare_log_csv.py"),ignore.stdout=T,wait = T)
-
+#convert all data to the appropriate format for analysis
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"docking_pre_analysis.R ",part_analysis),ignore.stdout=T,wait = T)
+#calculate interactions between receptor and ligands
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"docking_interactions.R ",part_analysis),ignore.stdout=T,wait = T)
-
+#calculate RMSD between all structures in the groups
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"RMSD_docking_group_structure.R ",part_analysis),ignore.stdout=T,wait = T)
+#make a calibration graph of RMSD cutoff
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"calibration_group_structure.R ",part_analysis),ignore.stdout=T,wait = T)
+#sort structures in the groups
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"docking_group_structure.R ",part_analysis,",",1),ignore.stdout=T,wait = T)
 
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"RMSD_merge_docking_parts.R ",part_analysis),ignore.stdout=T,wait = T)
+#make a calibration graph of RMSD cutoff
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"calibration_merge_structure.R ",part_analysis),ignore.stdout=T,wait = T)
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"merge_docking_parts.R ",part_analysis),ignore.stdout=T,wait = T)
 system(command = paste0("Rscript --vanilla  ",part_scriprs,"merge_interactions.R ",part_analysis),ignore.stdout=T,wait = T)
