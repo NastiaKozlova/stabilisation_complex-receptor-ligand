e097e805f0febf4b08ab739ed87838382d7c92a3
NastiaKozlova <kozlovaas.93gmail.com>
2023-05-26 14:01:38 -0500

    add tabulation

--- r_scripts/docking/r_scripts/RMSD_merge_docking_parts.R
+++ r_scripts/docking/r_scripts/RMSD_merge_docking_parts.R
@@ -1,9 +1,10 @@
 part_analysis <- commandArgs(trailingOnly=TRUE)
-
+#group ligand structures
 #group ligand structures
 library(bio3d)
 library(dplyr)
 library(ggplot2)
+v_rmsd<-3.5
 
 print(Sys.time())
 
@@ -15,111 +16,83 @@ df_all<-df_all%>%mutate(y=NA)
 df_all<-df_all%>%mutate(z=NA)
 i<-1
 for (i in 1:nrow(df_all)) {
-  a<-strsplit(df_all$center[i],split = "_")[[1]]
-  df_all$x[i]<-as.numeric(a[3])
-  df_all$y[i]<-as.numeric(a[5])
-  df_all$z[i]<-as.numeric(a[7])
+   a<-strsplit(df_all$center[i],split = "_")[[1]]
+   df_all$x[i]<-as.numeric(a[3])
+   df_all$y[i]<-as.numeric(a[5])
+   df_all$z[i]<-as.numeric(a[7])
 }
 df_all<-df_all%>%filter(!is.na(x))
 df_all$name<-NULL
 
-df_active_center<-read.csv("active_center.csv",stringsAsFactors = F)
-df_active_center<-left_join(df_all,df_active_center,by=c("center"="type"))
+part<-paste0(part_analysis,"din/")
+setwd(part)
+if(dir.exists(paste0(part,"fin_merged"))) {system(command = paste0("rm -r ",part,"fin_merged"),ignore.stdout=T,wait = T)}
+if(dir.exists(paste0(part,"structure_merged"))) {system(command = paste0("rm -r ",part,"structure_merged"),ignore.stdout=T,wait = T)}
+if(dir.exists(paste0(part,"groups_merged"))) {system(command = paste0("rm -r ",part,"groups_merged"),ignore.stdout=T,wait = T)}
+if (!dir.exists("RMSD_merged")) {dir.create("RMSD_merged")}
+if (!dir.exists("groups_merged")) {dir.create("groups_merged")}
+if (!dir.exists("structure_merged")) {dir.create("structure_merged")}
+if (!dir.exists("fin_merged")) {dir.create("fin_merged")}
 
-df_analysis<-df_all%>%select(receptor,ligand)
-df_analysis<-unique(df_analysis)
-df_analysis<-df_analysis%>%mutate(receptor_ligand=paste(receptor,ligand,sep = "_"))
-#v_receptor<-unique(df_all$receptor)
-j<-1
-for (j in 1:nrow(df_analysis)) {
-  if(!file.exists(paste0(part_analysis,"din/","RMSD_merged/",df_analysis$receptor_ligand[j],".csv"))){
-    df_all_sorted<-df_all%>%filter(receptor==df_analysis$receptor[j])
-    df_all_sorted<-df_all_sorted%>%filter(ligand==df_analysis$ligand[j])
-    pdb<-read.pdb(paste0(part_analysis,"receptor_start/",df_analysis$receptor[j],".pdb"))
-    df_pdb<-pdb$atom
-    df_pdb<-df_pdb%>%filter(elety=="CA")
-    part<-paste0(part_analysis,"din/")
-    setwd(part)
-    if (!dir.exists("RMSD_merged")) {dir.create("RMSD_merged")}
-    
-    df_all_sorted<-df_all_sorted%>%mutate(x_min=NA)
-    df_all_sorted<-df_all_sorted%>%mutate(x_max=NA)
-    
-    df_all_sorted<-df_all_sorted%>%mutate(y_min=NA)
-    df_all_sorted<-df_all_sorted%>%mutate(y_max=NA)
-    
-    df_all_sorted<-df_all_sorted%>%mutate(z_min=NA)
-    df_all_sorted<-df_all_sorted%>%mutate(z_max=NA)
-    
-    for (i in 1:nrow(df_all_sorted)) {
-      df_active<-df_active_center%>%filter(center==df_all_sorted$center[i])
-      df_pdb_a<-df_pdb[df_pdb$resno%in%df_active$resno,]
-      df_all_sorted$x_min[i]<-min(df_pdb_a$x)-5
-      df_all_sorted$x_max[i]<-max(df_pdb_a$x)+5
-      
-      df_all_sorted$y_min[i]<-min(df_pdb_a$y)-5
-      df_all_sorted$y_max[i]<-max(df_pdb_a$y)+5
-      
-      df_all_sorted$z_min[i]<-min(df_pdb_a$z)-5
-      df_all_sorted$z_max[i]<-max(df_pdb_a$z)+5
-    }
-    df_all_sorted_a<-df_all_sorted%>%left_join(df_all_sorted,df_all_sorted,by=c("receptor", "ligand"))
-    
-    
-    df_all_sorted_a<-df_all_sorted_a%>%filter(x.x <=x.y)
-    df_all_sorted_a<-df_all_sorted_a%>%filter(y.x <=y.y)
-    df_all_sorted_a<-df_all_sorted_a%>%filter(y.x <=z.y)
-    
-    
-    df_all_sorted_a<-df_all_sorted_a%>%filter(x_min.x <=x_min.y)
-    df_all_sorted_a<-df_all_sorted_a%>%filter(y_min.x <=y_min.y)
-    df_all_sorted_a<-df_all_sorted_a%>%filter(y_min.x <=z_min.y)
-    
-    df_all_sorted_a<-df_all_sorted_a%>%filter(x_max.x >x_min.y)
-    df_all_sorted_a<-df_all_sorted_a%>%filter(y_max.x >y_min.y)
-    df_all_sorted_a<-df_all_sorted_a%>%filter(z_max.x >z_min.y)
-    df_all_sorted_a<-unique(df_all_sorted_a)
-    df_all_sorted_a<-df_all_sorted_a%>%select(receptor,ligand,center.x,center.y)
-    
-    v_structure<-list.files(paste0(part_analysis,"din/str_fin"))
-    df_structure<-data.frame(matrix(nrow=length(v_structure),ncol = 4))
-    colnames(df_structure)<-c("name","receptor","ligand","center")
-    df_structure$name<-v_structure
-    for (i in 1:nrow(df_all_sorted)) {
-      df_structure$receptor[grepl(x = df_structure$name,pattern = df_all_sorted$receptor[i])]<-df_all_sorted$receptor[i]
-      df_structure$ligand[grepl(x = df_structure$name,pattern = df_all_sorted$ligand[i])]<-df_all_sorted$ligand[i]
-      df_structure$center[grepl(x = df_structure$name,pattern = df_all_sorted$center[i])]<-df_all_sorted$center[i]
-    }
-    
-    df_structure<-df_structure%>%filter(!is.na(center))
-    #   df_structure<-df_structure
-    df_all_sorte<-left_join(df_all_sorted_a,df_structure,by=c("receptor","ligand","center.x"="center"))
-    df_all_sorte<-left_join(df_all_sorte,df_structure,by=c("receptor","ligand","center.y"="center"))
-    df_all_sorte<-df_all_sorte%>%filter(!is.na(name.x))
-    df_all_sorte<-df_all_sorte%>%filter(!is.na(name.y))
-    
-    df_all_sorte<-df_all_sorte%>%mutate(RMSD=NA)
-    df_all_sorte_start<-df_all_sorte%>%filter(!is.na(RMSD))
-    print(paste(df_analysis$receptor_ligand[j],Sys.time()))
-    v_sorte<-unique(df_all_sorte$center.x)
-    if(nrow(df_all_sorte)>0){
-      for (p in 1:length(v_sorte)) {
-        df_all_sorte_add<-df_all_sorte%>%filter(center.x==v_sorte[p])
-        df_all_sorte<-df_all_sorte%>%filter(center.x!=v_sorte[p])
-        
-        for (i in 1:nrow(df_all_sorte_add)) {
-          pdb_1<-read.pdb(paste0(part_analysis,"din/str_fin/",df_all_sorte_add$name.x[i]))
-          pdb_2<-read.pdb(paste0(part_analysis,"din/str_fin/",df_all_sorte_add$name.y[i]))
-          
-          df_all_sorte_add$RMSD[i]<-rmsd(pdb_1,pdb_2)
-        }
-        df_all_sorte_add<-df_all_sorte_add%>%filter(RMSD<50)
-        df_all_sorte_start<-rbind(df_all_sorte_start,df_all_sorte_add)
-      }
-      print(paste(df_analysis$receptor_ligand[j],Sys.time()))
-      write.csv(df_all_sorte_start,paste0("RMSD_merged/",df_analysis$receptor_ligand[j],".csv"),row.names=F)
-    }
-  }
+
+v_structure<-list.files(paste0(part_analysis,"din/str_fin"))
+df_structure<-data.frame(matrix(nrow=length(v_structure),ncol = 5))
+colnames(df_structure)<-c("name","receptor","ligand","center","RMSD")
+df_structure$name<-v_structure
+for (j in 1:nrow(df_structure)) {
+   df_structure$receptor[j]<-strsplit(x = df_structure$name[j],split = "_")[[1]][1]
+   df_structure$ligand[j]<-strsplit(x = df_structure$name[j],split = "_")[[1]][2]
 }
+v_center<-unique(df_all$center)
+i<-1
+for (i in 1:length(v_center)) {
+   df_structure$center[grepl(x = df_structure$name,pattern = v_center[i])]<-v_center[i]
+}
+df_structure<-df_structure%>%filter(!is.na(center))
 
+df_structure<-left_join(df_structure,df_all,by=c("receptor", "ligand", "center"))
+
+df_analysis<-df_structure%>%select(receptor,ligand)
+df_analysis<-unique(df_analysis)
+df_analysis<-df_analysis%>%mutate(receptor_ligand=paste0(receptor,"_",ligand))
+q<-1
+i<-1
+print(Sys.time())
+
+for (q in 1:nrow(df_analysis)) {
+   if(!file.exists(paste0("RMSD_merged/",df_analysis$receptor_ligand[q],".csv"))){
+      df_structure_TEMP<-df_structure%>%filter(receptor==df_analysis$receptor[q])
+      df_structure_TEMP<-df_structure_TEMP%>%filter(ligand==df_analysis$ligand[q])
+      
+      v_RMSD<-unique(df_structure_TEMP$center)
+      df_structure_TEMPA<-df_structure_TEMP%>%filter(center==v_RMSD[1])
+      df_structure_merge<-left_join(df_structure_TEMPA,df_structure_TEMP,by=c("receptor", "ligand","RMSD"))
+      df_structure_merge_start<-df_structure_merge%>%filter(!is.na(RMSD))
+      for (i in 1:length(v_RMSD)) {
+         df_structure_TEMPA<-df_structure_TEMP%>%filter(center==v_RMSD[i])
+         df_structure_merge<-left_join(df_structure_TEMPA,df_structure_TEMP,by=c("receptor", "ligand","RMSD"))
+         df_structure_merge<-df_structure_merge%>%filter(x.x<=x.y)
+         df_structure_merge<-df_structure_merge%>%filter(y.x<=y.y)
+         df_structure_merge<-df_structure_merge%>%filter(z.x<=z.y)
+         
+         df_structure_merge<-df_structure_merge%>%filter((x.x+30)>=x.y)
+         df_structure_merge<-df_structure_merge%>%filter((y.x+30)>=y.y)
+         df_structure_merge<-df_structure_merge%>%filter((z.x+30)>=z.y)
+         df_structure_merge_start<-rbind(df_structure_merge_start,df_structure_merge)
+         
+      }
+      if(nrow(df_structure_merge_start)>0){
+         for (j in 1:nrow(df_structure_merge_start)) {
+            pdb_1<-read.pdb(paste0("str_fin/",df_structure_merge_start$name.x[j]))
+            pdb_2<-read.pdb(paste0("str_fin/",df_structure_merge_start$name.y[j]))
+            
+            df_structure_merge_start$RMSD[j]<-rmsd(pdb_1,pdb_2)
+         }
+         df_structure_merge_start<-df_structure_merge_start%>%filter(RMSD<50)
+      }
+      
+      write.csv(df_structure_merge_start,paste0("RMSD_merged/",df_analysis$receptor_ligand[q],".csv"),row.names=F)
+      print(paste(df_analysis$receptor_ligand[q],Sys.time()))
+   }
+}
 print(Sys.time())
