1fbc5aa3b92c244c78b45c6142e6ad8911c30121
NastiaKozlova <kozlovaas.93gmail.com>
2023-10-11 11:23:55 -0500

    fix bugs

--- r_scripts/docking/r_scripts/docking_group_center.R
+++ r_scripts/docking/r_scripts/docking_group_center.R
@@ -2,7 +2,8 @@ part_analysis <- commandArgs(trailingOnly=TRUE)
 #group ligand structures
 part_TEMP<-strsplit(part_analysis,split = ",")[[1]]
 part_start<-part_TEMP[1]
-v_rmsd<-as.numeric(part_TEMP[2])
+v_rmsd<-10
+v_str<-0
 library(bio3d)
 library(readr)
 library(dplyr)
@@ -14,26 +15,14 @@ setwd(part)
 if(dir.exists(paste0(part,"groups"))) {system(command = paste0("rm -r ",part,"groups"),ignore.stdout=T,wait = T)}
 if(dir.exists(paste0(part,"groups_fin"))) {system(command = paste0("rm -r ",part,"groups_fin"),ignore.stdout=T,wait = T)}
 if(dir.exists(paste0(part,"str"))) {system(command = paste0("rm -r ",part,"str"),ignore.stdout=T,wait = T)}
-if(dir.exists(paste0(part,"str_fin"))) {system(command = paste0("rm -r ",part,"str_fin"),ignore.stdout=T,wait = T)}
+if(dir.exists(paste0(part,"str_fin_center"))) {system(command = paste0("rm -r ",part,"str_fin_center"),ignore.stdout=T,wait = T)}
 if (!dir.exists("groups")) {dir.create("groups")}
-
+if (!dir.exists("groups_fin")) {dir.create("groups_fin")}
+if (!dir.exists("str_fin_center")) {dir.create("str_fin_center")}
 df_all<-read.csv(paste0(part_start,"df_all.csv"),stringsAsFactors = F)
 df_all<-df_all%>%mutate(name=paste0(receptor,"_",ligand,"_",center))
-df_all<-df_all%>%mutate(x=NA)
-df_all<-df_all%>%mutate(y=NA)
-df_all<-df_all%>%mutate(z=NA)
-i<-1
-for (i in 1:nrow(df_all)) {
-  a<-strsplit(df_all$center[i],split = "_")[[1]]
-  df_all$x[i]<-as.numeric(a[3])
-  df_all$y[i]<-as.numeric(a[5])
-  df_all$z[i]<-as.numeric(a[7])
-}
-df_all<-df_all%>%filter(is.na(x))
+df_all<-df_all%>%mutate(stuture_number=NA)
 #sort to grops
-i<-1
-#df_all<-df_all%>%filter(ligand=="Propidium")
-#df_all<-df_all[df_all$center%in%c("cat_trio","anion_cite","perefer_anion_cite","acyl_poket","hydroxyanion_hole"),]
 for (i in 1:nrow(df_all)) {
   
   if(file.exists(paste0("RMSD_analysis/",df_all$name[i],".csv"))){
@@ -41,10 +30,11 @@ for (i in 1:nrow(df_all)) {
     if (!dir.exists(paste0("groups/",df_all$name[i]))) {dir.create(paste0("groups/",df_all$name[i]))}
     df_RMSD_all<-read.csv(paste0("RMSD_analysis/",df_all$name[i],".csv"),stringsAsFactors = F)
     df_RMSD_all<-df_RMSD_all%>%filter(RMSD<v_rmsd)
+    df_all$stuture_number[i]<-nrow(df_RMSD_all)
     df_RMSD_all<-df_RMSD_all%>%group_by(models.x)%>%mutate(number=n())
     df_RMSD_all<-ungroup(df_RMSD_all)                                             
-    df_RMSD_all<-df_RMSD_all%>%filter(number>5)
-    if (nrow(df_RMSD_all)>0){
+    df_RMSD_all<-df_RMSD_all%>%filter(number>v_str)
+    if (nrow(df_RMSD_all)>v_str){
       df_RMSD<-df_RMSD_all%>%select(models.x,number)
       df_RMSD<-unique(df_RMSD)
       df_RMSD<-df_RMSD%>%arrange(desc(number))
@@ -52,7 +42,7 @@ for (i in 1:nrow(df_all)) {
       for (j in 1:nrow(df_RMSD)) {
         if (!is.na(df_RMSD$models.x[j])) {
           df_RMSD_all_test<-df_RMSD_all%>%filter(models.x==df_RMSD$models.x[j])
-          if (nrow(df_RMSD_all_test)>5) {
+          if (nrow(df_RMSD_all_test)>0) {
             df_RMSD_all_test<-df_RMSD_all_test%>%mutate(grop_number=j)
             write.csv(df_RMSD_all_test,paste0("groups/",df_all$name[i],"/grop_",j,".csv"),row.names = F) 
           }
@@ -68,7 +58,8 @@ for (i in 1:nrow(df_all)) {
     }
   }
 }
-
+#ggplot(data=df_all)+
+#geom_freqpoly(aes(x=stuture_number))
 
 #combine all groups logs files
 i<-3
@@ -106,7 +97,7 @@ for (j in 1:length(df_all$name)) {
     for (q in 1:nrow(df_RMSD)){
       pdb<-read.pdb(paste0("pdb_second/",df_RMSD$ligand_center[q],"/",df_RMSD$models.y[q]))
       
-      write.pdb(pdb,paste0("str_fin/",df_RMSD$ligand_center[q],"_",df_RMSD$grop_number[q],"_",df_RMSD$models.y[q]))
+      write.pdb(pdb,paste0("str_fin_center/",df_RMSD$ligand_center[q],"_",df_RMSD$grop_number[q],"_",df_RMSD$models.y[q]))
     }
   }
 }
@@ -114,22 +105,25 @@ for (j in 1:length(df_all$name)) {
 
 #energy bonding
 i<-1
-if (!dir.exists("log_fin")) {dir.create("log_fin")}
+if (!dir.exists("log_fin_center")) {dir.create("log_fin_center")}
 if (!dir.exists("plot")) {dir.create("plot")}
-df_log<-read.csv("df_log_all.csv",stringsAsFactors = F)
+df_log<-read.csv("df_log_all_center.csv",stringsAsFactors = F)
 df_log<-df_log%>%mutate(models.y=paste0("frame_",new_number,".pdb"))
 for (i in 1:nrow(df_all)) {
   if(file.exists(paste0("groups_fin/",df_all$name[i],".csv"))){
     df_groups<-read.csv(paste0("groups_fin/",df_all$name[i],".csv"),stringsAsFactors = F)
     df_fin<-left_join(df_groups,df_log,by=c("models.y","ligand_center"="name"))
-    write.csv(df_fin,paste0("log_fin/",df_all$name[i],".csv"),row.names = F)
+    write.csv(df_fin,paste0("log_fin_center/",df_all$name[i],".csv"),row.names = F)
   }
 }
-df_fin<-read.csv(paste0("log_fin/",df_all$name[1],".csv"),stringsAsFactors = F)
+a<-list.files("log_fin_center")
+df_all<-df_all[paste0(df_all$name,".csv")%in%a,]
+
+df_fin<-read.csv(paste0("log_fin_center/",df_all$name[1],".csv"),stringsAsFactors = F)
 for (i in 2:length(df_all$name)) {
   if(file.exists(paste0("groups_fin/",df_all$name[i],".csv"))){
-    df_fin_add<-read.csv(paste0("log_fin/",df_all$name[i],".csv"),stringsAsFactors = F)
+    df_fin_add<-read.csv(paste0("log_fin_center/",df_all$name[i],".csv"),stringsAsFactors = F)
     df_fin<-rbind(df_fin,df_fin_add)
   }
 }
-write.csv(df_fin,"log_fin.csv",row.names = F)
+write.csv(df_fin,"log_fin_center.csv",row.names = F)
