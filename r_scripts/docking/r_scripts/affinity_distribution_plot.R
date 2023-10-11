7d922e3ba70edb2f5413c81e4f722bce9fa329a4
NastiaKozlova <kozlovaas.93gmail.com>
2023-10-11 11:24:16 -0500

    change plot

--- r_scripts/docking/r_scripts/affinity_distribution_plot.R
+++ r_scripts/docking/r_scripts/affinity_distribution_plot.R
@@ -44,12 +44,28 @@ p<-ggboxplot(df_structure_RMSD, x = "receptor_ligand", y = "affinity") +
 ggsave(p,file="boxplot_ligand_affility.png", width = 24, height = 15, units = c("cm"), dpi = 200 )
 colnames(pwc2)
 pwc2<-pwc2%>%select(group1,group2,p.adj,p.adj.signif)
-p<-ggplot(data=df_structure_RMSD, aes(colour = receptor_ligand, x = affinity)) +
+df_structure_RMSD_anti<-df_structure_RMSD[df_structure_RMSD$ligand%in%c("chloramphenicol", "norfloxacin"),]
+p<-ggplot(data=df_structure_RMSD_anti, aes(colour = receptor_ligand, x = affinity)) +
    geom_density()+theme_bw()+
+  scale_x_continuous(breaks = seq(round(min(df_structure_RMSD$affinity),digits = 0),round(max(df_structure_RMSD$affinity),digits = 0)),
+                     labels = seq(round(min(df_structure_RMSD$affinity),digits = 0),round(max(df_structure_RMSD$affinity),digits = 0)))+
    theme(legend.position = "bottom")
 p_table<-p+annotate(geom = "table",
                   x = 0,
                   y = 0.5,
                   label = list(pwc2))
 #pwc2
-ggsave(p_table,file="density_ligand_affility.png", width = 24, height = 15, units = c("cm"), dpi = 200 )
+ggsave(p,file="density_ligand_affility_antibiotics.png", width = 24, height = 15, units = c("cm"), dpi = 200 )
+
+df_structure_RMSD_lipids<-df_structure_RMSD[!df_structure_RMSD$ligand%in%c("chloramphenicol", "norfloxacin"),]
+p<-ggplot(data=df_structure_RMSD_lipids, aes(colour = receptor_ligand, x = affinity)) +
+  geom_density()+theme_bw()+
+  scale_x_continuous(breaks = seq(round(min(df_structure_RMSD$affinity),digits = 0),round(max(df_structure_RMSD$affinity),digits = 0)),
+                     labels = seq(round(min(df_structure_RMSD$affinity),digits = 0),round(max(df_structure_RMSD$affinity),digits = 0)))+
+  theme(legend.position = "bottom")
+p_table<-p+annotate(geom = "table",
+                    x = 0,
+                    y = 0.5,
+                    label = list(pwc2))
+#pwc2
+ggsave(p,file="density_ligand_affility_lipids.png", width = 24, height = 15, units = c("cm"), dpi = 200 )
