ad1dbb98570ecfc8615abc0f1c310ca4f6c11847
NastiaKozlova <kozlovaas.93gmail.com>
2023-10-27 13:38:38 -0500

    fix copy files

--- r_scripts/copy_files_for_MD_receptor_ligand.R
+++ r_scripts/copy_files_for_MD_receptor_ligand.R
@@ -11,8 +11,10 @@ if(!dir.exists(paste0(part_name,"programs/"))){dir.create(paste0(part_name,"prog
 if(!dir.exists(paste0(part_name,"start/"))){dir.create(paste0(part_name,"start/"))}
 if(!dir.exists(paste0(part_name,"start/ligands_surf/"))){dir.create(paste0(part_name,"start/ligands_surf/"))}
 if(!dir.exists(paste0(part_name,"start/ligands_center/"))){dir.create(paste0(part_name,"start/ligands_center/"))}
-if(!dir.exists(paste0(part_name,"start/receptor/"))){dir.create(paste0(part_name,"start/receptor/"))}
 system(command = paste0("cp -r ",part_start,"programs/NAMD_2.14_Linux-x86_64-multicore.tar.gz ",part_name,"programs/"),ignore.stdout=T,wait = T)
+
+if(!dir.exists(paste0(part_name,"start/receptor/"))){dir.create(paste0(part_name,"start/receptor/"))}
+system(command = paste0("cp ",part_start,v_list_protein,"/docking/docking_first/din/str_fin_center/* ",part_name,"start/ligands_center/"),ignore.stdout=T,wait = T)
 
 system(command = paste0("cp -r ",part_start,v_list_protein,"/docking/docking_first/receptor_start/start.pdb ", part_name,"start/receptor/start.pdb"))
 system(command = paste0("cp -r ",part_start,"programs/NAMD_2.14_Linux-x86_64-multicore.tar.gz ",part_name,"programs/"),ignore.stdout=T,wait = T)
