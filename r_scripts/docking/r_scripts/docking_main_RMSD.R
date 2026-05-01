97c5b6aff8c77b3f5f998c84d28400b37e0e8276
NastiaKozlova <kozlovaas.93gmail.com>
2026-05-01 14:39:08 -0500

    separate files

---
+++ r_scripts/docking/r_scripts/docking_main_RMSD.R
@@ -0,0 +1,15 @@
+part_protein <- commandArgs(trailingOnly=TRUE)
+part_TEMP<-strsplit(part_protein,split = ",",fixed = T)[[1]]
+part_start<-part_TEMP[1]
+v_list_protein<-part_TEMP[2]
+v_center<-part_TEMP[3]
+#check protein surface
+
+part_analysis<-paste0(part_start,v_list_protein,"/docking/docking_first/",v_center,"/")
+#part_analysis<-paste0(part_name,"docking_first/")
+part_scriprs<-paste0(part_start,"r_scripts/docking/r_scripts/")
+
+#calculate RMSD between all structures in the groups
+system(command = paste0("Rscript --vanilla  ",part_scriprs,"RMSD_docking_group_structure.R ",part_analysis),ignore.stdout=T,wait = T)
+#make a calibration graph of RMSD cutoff
+system(command = paste0("Rscript --vanilla  ",part_scriprs,"calibration_group_structure.R ",part_analysis),ignore.stdout=T,wait = T)
